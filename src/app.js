394336e6df60029634c1c7c5372afcdaefcf94cf
Nathaniel <nathanielbabalola1@gmail.com>
2022-08-26 08:49:12 +0100

    feat(src/ prettier and eslint files): initial project setup

---
+++ src/app.js
@@ -0,0 +1,32 @@
+// require dotenv to load environment variables
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const routes = require('./routes')
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    return res.send(
+        `Welcome 👋 To Air Quality Filter ${
+            new Date().toISOString().split('T')[0]
+        }`
+    )
+})
+
+app.use(routes)
+
+app.use((err, req, res, next) => {
+    const { message = 'Something went wrong', statusCode = 500 } = err
+    return res.status(statusCode).send({
+        success: false,
+        message: message,
+    })
+})
+
+module.exports = app
